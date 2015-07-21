d13f15c8825c02b9b09269639c80196092eb6afc
Peter Vandenberk <pvandenberk@mac.com>
2015-07-21 19:56:34 +0100

    Simplify things: random number of fields

--- lib/git/meta/settings.rb
+++ lib/git/meta/settings.rb
@@ -8,74 +8,73 @@ module Git
 
       module_function
 
-      def setting_status setting, value, valid, optional = false
+      def setting_status messages, valid, optional = false
         if Nike.interactive?
-          print "   %s - %s\x0d" % [setting, value]
+          print "   %s\x0d" % messages.join(' - ')
           sleep 0.75
           puts "%s" % (valid ? TICK : optional ? ARROW : CROSS)
         else
-          puts "%s  %s - %s" % [
+          puts "%s  %s" % [
             (valid ? TICK : optional ? ARROW : CROSS),
-            setting, value,
+            messages.join(' - '),
           ]
         end
       end
 
-      def file_status setting, file
+      def file_status message, file
         setting_status(
-          setting,
-          "%s - %s bytes" % [
+          [
+            message,
             Git::Meta.abbreviate(file),
-            File.size(file).commify
+            "#{File.size(file).commify} bytes",
           ],
           file && !file.empty? && File.file?(file),
           true
         )
       end
 
-      def directory_status setting, value, directory
+      def directory_status messages, directory
         setting_status(
-          setting,
-          value,
+          messages,
           directory && !directory.empty? && File.directory?(directory),
           false
         )
       end
 
-      def workarea_status setting, workarea
+      def workarea_status message, workarea
         directory_status(
-          setting,
-          "%s - %s repos" % [
+          [
+            message,
             Git::Meta.abbreviate(workarea, :workarea),
-            Dir.new(workarea).git_repos.count.commify
+            "#{Dir.new(workarea).git_repos.count.commify} repos"
           ],
           workarea
         )
       end
 
       def user_status user
-        setting_status("User", user, user && !user.empty?)
+        setting_status(["User", user], user && !user.empty?)
       end
 
       def organization_status orgs
         for org in orgs
-          setting_status("Organization", org, org && !org.empty?, true)
-          setting_status("Organization", "member?", Git::Meta::ORGS.include?(org), optional = !Git::Meta.connected?)
+          setting_status(["Organization", org], org && !org.empty?, true)
+          setting_status(["Organization", "member?"], Git::Meta::ORGS.include?(org), optional = !Git::Meta.connected?)
         end
       end
 
       def token_status token
-        setting_status("Token", token, token && !token.empty?)
-        setting_status("Token", "valid?", token && !token.empty? && Git::Meta::LOGIN, optional = !Git::Meta.connected?)
-        setting_status("Token", "owned by #{Git::Meta::USER}?", Git::Meta::LOGIN == Git::Meta::USER, optional = !Git::Meta.connected?)
+        setting_status(["Token", token], token && !token.empty?)
+        setting_status(["Token", "valid?"], token && !token.empty? && Git::Meta::LOGIN, optional = !Git::Meta.connected?)
+        setting_status(["Token", "owned by #{Git::Meta::USER}?"], Git::Meta::LOGIN == Git::Meta::USER, optional = !Git::Meta.connected?)
       end
 
       def home_status home
-        directory_status("Home", home, home)
+        directory_status(["Home", home], home)
       end
 
       def main_workarea_status workarea
-        directory_status("Workarea (main)", Git::Meta.abbreviate(workarea, :home), workarea)
+        directory_status(["Workarea (main)", Git::Meta.abbreviate(workarea, :home)], workarea)
       end
 
       def user_workarea_status user
