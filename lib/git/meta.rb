4b5843f9f8c964955209b52e6668f010c5f2aa7f
Peter Vandenberk <pvandenberk@mac.com>
2015-11-10 12:43:45 +0000

    Combine existence and size checking of cache file

--- lib/git/meta.rb
+++ lib/git/meta.rb
@@ -160,26 +160,22 @@ module Git
     end
 
     def repositories
-      if File.exist? YAML_CACHE
-        if (yaml_cache = File.read(YAML_CACHE)).empty?
-          refresh_repositories and repositories
-        else
-          @repositories ||= YAML.load(yaml_cache).tap do |projects|
-            projects.each_with_index do |project, index|
-              # ensure #method_missing works for Sawyer::Resource instances
-              project.instance_eval("@_metaclass = (class << self; self ; end)")
-              project.owner.instance_eval("@_metaclass = (class << self; self ; end)")
-              # adorn 'project', which is a Sawyer::Resource
-              project.parent_dir = File.join(WORKAREA, project.owner.login)
-              project.local_path = File.join(WORKAREA, project.full_name)
-              project.fractional_index = "#{index + 1}/#{projects.count}"
-              # ensure 'project' has handle on an Octokit client
-              def project.client() @client ||= Git::Meta.send(:client) ; end
-              # extend 'project' with 'just do it' capabilities
-              project.extend Nike
-              # extend 'project' with some cheeky knowledge
-              # project.extend Prometheus
-            end
+      if File.size? YAML_CACHE
+        @repositories ||= YAML.load(File.read(YAML_CACHE)).tap do |projects|
+          projects.each_with_index do |project, index|
+            # ensure #method_missing works for Sawyer::Resource instances
+            project.instance_eval("@_metaclass = (class << self; self ; end)")
+            project.owner.instance_eval("@_metaclass = (class << self; self ; end)")
+            # adorn 'project', which is a Sawyer::Resource
+            project.parent_dir = File.join(WORKAREA, project.owner.login)
+            project.local_path = File.join(WORKAREA, project.full_name)
+            project.fractional_index = "#{index + 1}/#{projects.count}"
+            # ensure 'project' has handle on an Octokit client
+            def project.client() @client ||= Git::Meta.send(:client) ; end
+            # extend 'project' with 'just do it' capabilities
+            project.extend Nike
+            # extend 'project' with some cheeky knowledge
+            # project.extend Prometheus
           end
         end
       else
