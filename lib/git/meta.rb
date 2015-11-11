2eb803cbc7a2ba7ff366e75a6c32d446378198cf
Peter Vandenberk <pvandenberk@mac.com>
2015-11-11 12:39:48 +0000

    Better way to give each project a `client` handle

--- lib/git/meta.rb
+++ lib/git/meta.rb
@@ -109,12 +109,12 @@ module Git
             # ensure #method_missing works for Sawyer::Resource instances
             project.instance_eval("@_metaclass = (class << self; self ; end)")
             project.owner.instance_eval("@_metaclass = (class << self; self ; end)")
+            # ensure 'project' has handle on an Octokit client
+            project.client = Git::Hub.send(:client)
             # adorn 'project', which is a Sawyer::Resource
             project.parent_dir = File.join(WORKAREA, project.owner.login)
             project.local_path = File.join(WORKAREA, project.full_name)
             project.fractional_index = "#{index + 1}/#{projects.count}"
-            # ensure 'project' has handle on an Octokit client
-            def project.client() @client ||= Git::Hub.send(:client) ; end
             # extend 'project' with 'just do it' capabilities
             project.extend Nike
           end
