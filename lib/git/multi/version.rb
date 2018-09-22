2dd774c54b26991e57e7d2b167f51df920ececde
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2018-09-22 09:13:22 +0100

    Psych no longer relevant, but include other deps

--- lib/git/multi/version.rb
+++ lib/git/multi/version.rb
@@ -1,21 +1,24 @@
 require 'octokit'
 require 'sawyer'
-require 'psych'
+require 'faraday'
+require 'addressable'
 
 module Git
   module Multi
     NAME = 'git-multi'
     VERSION = '1.0.5'
 
-    LONG_VERSION = "%s v%s (%s v%s, %s v%s, %s v%s)" % [
+    LONG_VERSION = "%s v%s (%s v%s, %s v%s, %s v%s, %s v%s)" % [
       NAME,
       VERSION,
       'octokit.rb',
       Octokit::VERSION,
       'sawyer',
       Sawyer::VERSION,
-      'psych',
-      Psych::VERSION,
+      'faraday',
+      Faraday::VERSION,
+      'addressable',
+      Addressable::VERSION::STRING,
     ]
   end
 end
