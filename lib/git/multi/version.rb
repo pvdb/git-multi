e486232f3f798dabdabe8e5e833e674314b2c8d9
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2018-11-01 16:25:17 +0000

    Bump version for new release

--- lib/git/multi/version.rb
+++ lib/git/multi/version.rb
@@ -6,7 +6,7 @@ require 'addressable'
 module Git
   module Multi
     NAME = 'git-multi'.freeze
-    VERSION = '1.0.9'.freeze
+    VERSION = '1.0.10'.freeze
 
     DEPENDENCY_VERSIONS = [
       "octokit.rb v#{Octokit::VERSION}",
