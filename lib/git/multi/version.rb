de49d60b1062dcfce5bc93d411e60c6d7e66e9a3
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2018-11-09 16:42:57 +0000

    Cater for no command-line args

--- lib/git/multi/version.rb
+++ lib/git/multi/version.rb
@@ -6,7 +6,7 @@ require 'addressable'
 module Git
   module Multi
     NAME = 'git-multi'.freeze
-    VERSION = '1.2.0'.freeze
+    VERSION = '1.2.1'.freeze
 
     DEPENDENCY_VERSIONS = [
       "octokit.rb v#{Octokit::VERSION}",
